4b4426b63cee2b2fcc920768751b616c494ae652
Ashina Shanuka <45147260+ashinashanuka@users.noreply.github.com>
2019-10-02 12:47:01 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+rint("*****")
+name = input('Name:')
+print('what is your age',(name),'?')
+days = age*365
+minutes = age*1002
+seconds = age*100000
+
+print('has been live for',days,'days',minutes,'minutes',seconds,'seconds')
